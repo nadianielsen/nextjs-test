5c08c9824205bc3003ae882e3975fa469a7f6ffc
nadianielsen <U2404561@rts-365.dk>
2023-04-14 12:10:42 +0200

    hover effect on mobile and desktop change

--- app/components/Navigation.js
+++ app/components/Navigation.js
@@ -28,8 +28,8 @@ const Navigation = () => {
         </div>
         <nav className={` ${isOpen ? "flex" : "hidden"} absolute top-full left-0 bg-red-600/75 backdrop-blur-[2px] w-screen py-4 flex-col gap-5 items-center md:bg-transparent md:static md:w-auto md:flex-row md:flex md:px-5 md:pt-8`}>
             <Link href="/" className={`${montserrat.className} text-2xl hidden md:block font-bold`}>Movie<span className="text-red-600">Base</span></Link>
-            <Link href="/movies" className="hover:text-red-600 transition-all">Movies</Link>
-            <Link href="/tvseries" className="hover:text-red-600 transition-all">Tv Series</Link>
+            <Link href="/movies" className="hover:text-neutral-400 md:hover:text-red-600 transition-all">Movies</Link>
+            <Link href="/tvseries" className="hover:text-neutral-400 md:hover:text-red-600 transition-all">Tv Series</Link>
         </nav>
         </>
      );
