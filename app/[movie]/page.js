07efeec352834d3c19db5208f26ac8b2692a079f
nadianielsen <U2404561@rts-365.dk>
2023-04-18 12:32:34 +0200

    just added some ratings and made the play trailer button responsive

--- app/[movie]/page.js
+++ app/[movie]/page.js
@@ -1,5 +1,5 @@
 import Image from "next/image"
-import { FaCheckCircle , FaPlay} from "react-icons/fa"
+import { FaCheckCircle , FaPlay, FaStar} from "react-icons/fa"
 
 export async function generateStatic() {
     const data = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}`)
@@ -36,8 +36,14 @@ const minutes = res.runtime % 60;
                         <h2 className="font-semibold flex gap-1">{res.status} <FaCheckCircle className="mt-1 text-green-500"/></h2>
                         <h2 className="">{res.release_date}</h2>
                         <h2 className="">{hours + "h" + minutes + "min"}</h2>
+                        <h2 className="hidden sm:flex gap-x-2"><FaStar className="text-yellow-400 mt-0.5 text-xl"/>{res.vote_average}</h2>
                         <button className="bg-red-600 hidden p-2 sm:flex gap-x-2 items-center rounded sm:relative sm:bottom-2">Play Trailer <FaPlay className="mt-1 text-sm"/> </button>
                     </div>
+                    <div className="flex gap-x-10">
+                        <h2 className="flex gap-x-2 sm:hidden"><FaStar className="text-yellow-400 mt-0.5 text-xl"/>{res.vote_average}</h2>
+                        <button className="bg-red-600 p-2 flex gap-x-2 items-center rounded relative bottom-2 sm:hidden">Play Trailer <FaPlay className="mt-1 text-sm"/> </button>
+                    </div>
+
                     <p className="md:w-[50%]">{res.overview}</p>
                     <div>
                     </div>
